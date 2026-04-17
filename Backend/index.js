a38d5bb3dc543be6a408a917c71cece66b24e67f
navneetnic28 <149612897+navneetnic28@users.noreply.github.com>
2026-04-17 16:34:29 +0530

    dmljk

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,40 @@
 import express from 'express';
-
-
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
 const app=express();
-const PORT =5001
-app.listen(PORT)
+
+
+app.get("/",(req,res)=> {
+    return res.status(200).json({
+        message:"welcome to the api",
+        timestamp:new Date().toISOString(),
+        success:true
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+const corsOptions ={
+    origin:["http://localhost:5121"],
+    Credentials:true,
+};
+app.use(cors(corsOptions));
+
+const PORT =process.env.PORT  || 8801
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
